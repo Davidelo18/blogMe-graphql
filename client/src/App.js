7933328b960a2b95ef0e7976d6eafbb73d9369ac
Dawid <daw.kielbik@gmail.com>
2021-05-09 00:56:52 +0200

    Dodanie pobierania postów

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-import './styles/App.scss';
+import './styles/root.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Route exact path="/" component={Home}/>
+    </Router>
   );
 }
 
